9a964a0bc8d5e0425f2f21b3f417fc1f3d84af37
Emo Kosov <emilkosov02@hotmail.com>
2023-08-19 13:40:46 +0300

    Optimize folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/auth/login/login.component';
-import { DetailsComponent } from './components/furniture/details/details.component';
-import { DashboardComponent } from './components/furniture/dashboard/dashboard.component';
-import { LogoutComponent } from './components/auth/logout/logout.component';
-import { RegisterComponent } from './components/auth/register/register.component';
-import { CreateComponent } from './components/furniture/create/create.component';
-import { EditComponent } from './components/furniture/edit/edit.component';
-import { MyFurnitureComponent } from './components/furniture/my-furniture/my-furniture.component';
-import { ProductDetailsResolver } from './shared/resolvers/details.resolver';
-import { CommentsResolver } from './shared/resolvers/comments.resolver';
-import { LikeCountResolver } from './shared/resolvers/like-count-resolver';
-import { isLikedResolver } from './shared/resolvers/is-liked-resolver';
 import { AuthGuard } from './shared/guards/auth-guard';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
   {
@@ -31,11 +19,11 @@ const routes: Routes = [
     data: {
       loginRequired: true
     },
-    loadChildren: () => import('./components/furniture/furniture.module').then(m => m.FurnitureModule)
+    loadChildren: () => import('./furniture/furniture.module').then(m => m.FurnitureModule)
   },
   {
     path: 'auth',
-    loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
   {
     path: '**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/auth/login/login.component';
-import { DetailsComponent } from './components/furniture/details/details.component';
-import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { CartItemComponent } from './components/furniture/cart-item/cart-item.component';
+import { HomeComponent } from './core/home/home.component';
+import { HeaderComponent } from './core/header/header.component';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthInterceptor } from 'src/interceptors/auth.interceptor';
-import { CommentsComponent } from './components/furniture/comments/comments.component';
-import { EditComponent } from './components/furniture/edit/edit.component';
-import { MyFurnitureComponent } from './components/furniture/my-furniture/my-furniture.component';
-import { CreateComponent } from './components/furniture/create/create.component';
-import { DashboardComponent } from './components/furniture/dashboard/dashboard.component';
-import { RegisterComponent } from './components/auth/register/register.component';
-import { LogoutComponent } from './components/auth/logout/logout.component';
-import { CreateCommentComponent } from './components/furniture/create-comment/create-comment.component';
-import { FurnitureModule } from './components/furniture/furniture.module';
 import { ProductDetailsResolver } from './shared/resolvers/details.resolver';
 import { CommentsResolver } from './shared/resolvers/comments.resolver';
 import { LikeCountResolver } from './shared/resolvers/like-count-resolver';
